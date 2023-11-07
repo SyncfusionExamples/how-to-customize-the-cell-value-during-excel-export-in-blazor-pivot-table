a03b50b08d418a2756e283c481a32c077d61f0f3
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-11-07 18:16:46 +0530

    Update the project

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -1,8 +1,8 @@
-namespace BlazorApp4.Data
+namespace CustomizingCellValue.Data
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
 
